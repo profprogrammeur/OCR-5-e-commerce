50ff825106d63fdc1f3918fc253c3142277356e0
profprogrammeur <contact@profprogrammeur.com>
2020-02-25 05:16:00 -0400

    encapsulation

--- js/index.js
+++ js/index.js
@@ -1,54 +1,47 @@
 "use strict";
 
-// var response = "";
-let response = "";
+// let response = "";
+// function setRequest(callback) {
 
-function setRequest(callback) {
-    let exportResp 
+function setRequest() {
+    let response = "";
     const request = new XMLHttpRequest();
-    request.onload = function  () {
+    request.onload = function () {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             response = JSON.parse(this.responseText);
             response.forEach(product => createProductList(product.name));
-            exportResp = response
-            console.log(exportResp)
-            callback(response)
+            // console.log(response)
+            // callback(response)
         }
-
     };
     request.open("GET", "http://localhost:3000/api/cameras/");
     request.send();
-    
+    // setRequest(afficheResponse);
+    // function afficheResponse () {console.log(response)}
 
-    return "exportResp"
+    function openProductPage() {
+        const numero = Array.from(event.target.parentNode.children).indexOf(event.target);
+        const _id = response[numero]._id;
+        location.href = "product.html" + "?_id=" + _id;
+    }
+
+    function createProductList(productName) {
+        const listGroup = document.getElementById("listGroup");
+        const anchorElement = document.createElement("a");
+        anchorElement.classList.add(
+            "mx-auto",
+            "w-50",
+            "text-center",
+            "list-group-item",
+            "list-group-item-action",
+            "list-group-item-info"
+        );
+        anchorElement.addEventListener("click", openProductPage);
+        anchorElement.appendChild(document.createTextNode(productName)); //makes text from input field the li text
+        listGroup.appendChild(anchorElement); //adds li to ul
+    }
+
 }
-
-let resp = setRequest(afficheResponse);
-
-function afficheResponse () {console.log(response)}
-
-function openProductPage() {
-    const numero = Array.from(event.target.parentNode.children).indexOf(event.target);
-    const _id = response[numero]._id;
-    location.href = "product.html" + "?_id=" + _id;
-}
-
-function createProductList(productName) {
-    const listGroup = document.getElementById("listGroup");
-    const anchorElement = document.createElement("a");
-    anchorElement.classList.add(
-        "mx-auto",
-        "w-50",
-        "text-center",
-        "list-group-item",
-        "list-group-item-action",
-        "list-group-item-info"
-    );
-    anchorElement.addEventListener("click", openProductPage);
-    anchorElement.appendChild(document.createTextNode(productName)); //makes text from input field the li text
-    listGroup.appendChild(anchorElement); //adds li to ul
-}
-
-
+setRequest();
 
 
